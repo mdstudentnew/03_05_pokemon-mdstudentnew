f26a9a29f2f5a89dc2e98d2a1b0f59d09e2ce81c
mdstudentnew <74984449+mdstudentnew@users.noreply.github.com>
2020-12-29 16:11:57 -0600

    Update Pokemon.java

--- Pokemon.java
+++ Pokemon.java
@@ -1,1 +1,60 @@
 
+
+
+public class Pokemon{
+  private double weight, catchRate;
+  private int level, attackPower;
+  private final double MAX_CATCH_RATE = 0.25;
+
+  public Pokemon(double theWeight, int theLevel){
+    weight = theWeight;
+    level = theLevel;
+    catchRate = Math.random()*MAX_CATCH_RATE;
+    attackPower = (int)(Math.random()*11)+60;
+  }
+
+public double getWeight(){
+  return weight;
+}
+
+public int getLevel(){
+  return level;
+}
+
+public double getCatchRate(){
+  return catchRate;
+}
+
+public int getAttackPower(){
+  return attackPower;
+}
+
+public void gainWeight(){
+  weight = weight *1.05;
+}
+
+public void workout(){
+  weight = weight * 0.95;
+}
+
+public void levelUp(){
+  level++;
+  attackPower = (int)(attackPower*1.05);
+}
+
+public String cry(){
+  return "Pokemon!";
+}
+
+public String toString(){
+  return "The Pokemon is level "+level+" and weighs "+weight+ "."; 
+}
+
+public boolean equals(Object obj){
+    Pokemon other = (Pokemon)obj; 
+    return this.level == other.level;
+}
+
+
+
+}
